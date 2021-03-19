66d42d0a89cc0207b097d65b50d20a5b96e4c9c6
bal3000 <bal3000286@googlemail.com>
2021-03-19 12:42:40 +0000

    header ws logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { ChromecastService } from '../../services/chromecast.service';
+import { addChromecast } from '../../state/actions/chromecasts.actions';
+import { selectChromecasts } from '../../state/selectors/chromecasts.selectors';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  chromecasts$ = this.store.pipe(select(selectChromecasts));
 
-  constructor() { }
+  constructor(
+    private readonly store: Store,
+    private readonly chromecastService: ChromecastService
+  ) {}
 
   ngOnInit(): void {
+    this.chromecastService.getChromecasts().subscribe(
+      (chromecast) => this.store.dispatch(addChromecast(chromecast)),
+      (err) => console.log(err),
+      () => console.log('complete')
+    );
   }
-
 }
--- src/app/services/chromecast.service.ts
+++ src/app/services/chromecast.service.ts
@@ -1,14 +1,14 @@
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { Injectable } from '@angular/core';
-import { Chromecast } from '../models/chromecast';
 import { Observable } from 'rxjs';
+import { Chromecast } from '../models/chromecast';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChromecastService {
   private chromecastWs: WebSocketSubject<Chromecast> = webSocket(
-    'ws://localhost:8080'
+    'ws://localhost:8080/chromecasts'
   );
 
   constructor() {}
